e87bd154e4959d918ef4a240db534d6f7aefc33f
Orest <orestdiduh20062gmail.com>
2025-01-18 02:02:01 +0200

    Оновлення package.json та vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'harris-johnsen-2',
+	base: '/harris-johnsen-2',
+	build: {
+		outDir: 'dist', // Папка для збірки
+	},
 })
